5e46f75bd1b3dfe19a5f9e9f8bb41640846ee31d
Isming <email@example.com>
2026-03-24 15:42:09 +0500

    masalar boshladim

---
+++ main.js
@@ -0,0 +1,84 @@
+// const pi =3
+// let R = prompt("radiusni kiritiong");
+// l = 2 * pi *R;
+// s = pi**2;
+// console.log(l,s);
+
+
+// let a = 4;
+// let b = 9;
+// let g = Math.sqrt(a * b);
+// console.log(g);
+ 
+// let x = 1
+// let y = 3 * x ** 6 - 6 * x ** 2 - 7;
+
+// console.log(y);
+
+
+// let y =prompt("son kiriting");
+// let sum = 120;
+// let x = 3;
+// s = (sum / x*2);
+// console.log(s);
+
+
+// let a = 3;
+// let b = 4;
+ 
+// let A = a;
+// let B = b;
+ 
+// let x = a;
+// let y = b;
+
+// let c = 25;
+// let a = 2;
+// let b = 5;
+// let A = b;
+// let B = a;
+// let f = (A,B)
+// console.log(f);
+
+
+// let numbr = 123;
+
+// let num = Math.floor(numbr / 10)%10
+// console.log(num);
+
+
+
+// let number = 1239;
+// let num = Math.floor(number / 100)%10
+// console.log(num);
+
+
+
+// let N = prompt("otgan sekundni kiriting");
+// let soat = Math.floor(N / 3600);
+// let minut = Math.floor((N % 3600) / 60);
+// let secund = N % 60;
+// console.log(soat);
+
+
+
+let a =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
